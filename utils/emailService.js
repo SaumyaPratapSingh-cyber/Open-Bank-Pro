27f1acc41e5fc8ef4bab3134c7496a91b6f63ab2
Saumya Pratap Singh <157036137+SaumyaPratapSingh-cyber@users.noreply.github.com>
2025-12-24 01:09:44 +0530

    Fix: Add SMTP connection timeouts to prevent server hanging

--- utils/emailService.js
+++ utils/emailService.js
@@ -5,7 +5,10 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 10000, // 10 seconds
+    socketTimeout: 10000, // 10 seconds
 });
 
 const sendEmail = async (to, subject, html) => {
